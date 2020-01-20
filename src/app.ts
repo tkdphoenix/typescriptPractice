9b80cc41fa3f8c7bcb864c33342ba7294d940a5e
Joel Grissom <joelgrissom5@gmail.com>
2020-01-19 23:54:17 -0700

    chore: saving point for generics

--- src/app.ts
+++ src/app.ts
@@ -1,161 +1,99 @@
-// Intersection types
-type Admin = {
-  name: string
-  privileges: string[]
+////// Generic Functions and Contraints //////
+
+// function merge(objA: object, objB: object) {
+//   return Object.assign(objA, objB)
+// }
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB)
 }
 
-type Employee = {
-  name: string
-  startDate: Date
+const mergedObj = merge({name: 'Joel', hobbies: ['Hiking']}, {age: 49})
+
+mergedObj.name
+console.log(mergedObj)
+
+/* prior to this, we could not get name or age, because these properties don't exist on type Object. 
+ * Now that it uses generics to make the intersection of T & U we can utilize all the properties of the mergedObj
+ */
+interface Lengthy {
+  length: number;
 }
 
-type ElevatedEmployee = Admin & Employee
-
-const e1: ElevatedEmployee = {
-  name: 'Joel',
-  privileges: ['create-server'],
-  startDate: new Date(),
-}
-
-// This could have been a series of interfaces since they are very similar
-
-type Combinable = string | number
-type Numeric = number | boolean
-
-// becomes number type because that is the only intersection
-type Universal = Combinable & Numeric
-
-/////// Type Guards ///////
-function add(a: Combinable, b: Combinable) {
-  if (typeof a === 'string' || typeof b === 'string') { // <-- type guard with `typeof` keyword
-    return a.toString() + b.toString()
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.';
   }
-  return a + b
+  return [element, descriptionText];
 }
 
-type UnknownEmployee = Employee| Admin
+console.log(countAndDescribe(['Sports', 'Cooking']));
 
-function printEmployeeInformation(emp: UnknownEmployee) {
-  console.log('Name: ' + emp.name)
-  if ('privileges' in emp) {
-    console.log('Privileges: ' + emp.privileges)
-  }
-  if ('startDate' in emp) {
-    console.log('Start Date: ' + emp.startDate)
-  }
+////// "keyof" Constraint //////
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return 'Value: ' + obj[key]
 }
 
-printEmployeeInformation(e1)
-printEmployeeInformation({name: 'Yesenia', startDate: new Date()})
+console.log(extractAndConvert({name: 'Joel'}, 'name'))
 
-class Car {
-  drive() {
-    console.log('Driving...')
-  }
-}
+////// Generic Classes //////
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = []
 
-class Truck {
-  drive() {
-    console.log('Driving a truck...')
+  addItem(item: T) {
+    this.data.push(item)
   }
 
-  loadCargo(amount: number) {
-    console.log('Loading cargo ...' + amount)
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+
+  getItems() {
+    return [...this.data]
   }
 }
 
-type Vehicle = Car | Truck
+const textStorage = new DataStorage<string>()
+textStorage.addItem('Joel')
+textStorage.addItem('Yesenia')
+textStorage.removeItem('Joel')
+console.log(textStorage.getItems())
 
-const v1 = new Car()
-const v2 = new Truck()
+const numberStorage = new DataStorage<number>()
 
-function useVehicle(vehicle: Vehicle) {
-  vehicle.drive()
-  if (vehicle instanceof Truck) { // Because this is a class this will work, but not for Interfaces
-    vehicle.loadCargo(1000)
-  }
+// const objStorage = new DataStorage<object>()
+// objStorage.addItem({name: 'Joel'})
+// objStorage.addItem({name: 'Yesenia'})
+// objStorage.removeItem({name: 'Joel'})
+// console.log(objStorage.getItems());
+/* The above won't work, unless you create a constant to hold the specific object for a person's name, 
+ * otherwise JS thinks it is a regular object, and returns -1, which gives unexpected results 
+ * with objects. It will remove the last person, because -1 means it starts at the end of the Array.
+ * This is why we are explicit in the top of the DataStorage class to only allow certain types.
+ */
+
+////// Generic Utility Types (Partial and Readonly) //////
+interface CourseGoal {
+  title: string
+  description: string
+  completeUntil: Date
 }
 
-useVehicle(v1)
-useVehicle(v2)
-
-////// Discriminated Unions //////
-interface Bird {
-  type: 'bird'
-  flyingSpeed: number
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+  ): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {}
+  courseGoal.title = title
+  courseGoal.description = description
+  courseGoal.completeUntil = date
+  return courseGoal as CourseGoal
 }
 
-interface Horse {
-  type: 'horse'
-  runningSpeed: number
-}
-
-type Animal = Bird | Horse
-
-function moveAnimal(animal: Animal) {
-  let speed
-  switch (animal.type) {
-    case 'bird':
-      speed = animal.flyingSpeed
-      break
-    case 'horse':
-      speed = animal.runningSpeed
-      break
-  }
-  console.log('Moving at speed: ' + speed)
-}
-
-moveAnimal({type: 'horse', runningSpeed: 10}) // interpreter knows that `runningSpeed` is not an available property on type `Bird`
-
-////// Type Casting //////
-// const userInputElement = <HTMLInputElement>document.getElementById('user-input')! // needs `!` at the end to ensure the compiler that this will never return null
-// const userInputElement = document.getElementById('user-input')! as HTMLInputElement // alertnative to <HTMLInputElement>
-const userInputElement = document.getElementById('user-input') // otherwise, this must be declared and issues clarified in if conditional below
-
-if (userInputElement) {
-  (userInputElement as HTMLInputElement).value = 'Hi there!'
-}
-
-////// Index Properties //////
-interface ErrorContainer { // { email 'Not a valid email', username: 'Must start with a character!'}
-  [prop: string]: string
-}
-
-const errorBag: ErrorContainer = {
-  email: 'Not a valid email!',
-  username: 'Must start with a capital character!'
-}
-
-////// Function Overloads //////
-function addAgain(a: number, b: number): number;
-function addAgain(a: string, b: string): string;
-function addAgain(a: string, b: number): string;
-function addAgain(a: number, b: string): string;
-// function overloads (the 4 lines above) clarify what potential types you expect when the code is converted to JS
-function addAgain(a: Combinable, b: Combinable) {
-  if (typeof a === 'string' || typeof b === 'string') { // <-- type guard with `typeof` keyword
-    return a.toString() + b.toString()
-  }
-  return a + b
-}
-
-const result = addAgain('Joel', 'Grissom')
-result.split(' ') // doesn't work, because it is of Combinable type, which doesn't have a .split() method
-
-////// Optional Chaining //////
-const fetchedUserData = {
-  id: 'u1',
-  name: 'Joel',
-  job: { title: 'CEO', description: 'My own company' }
-}
-
-console.log(fetchedUserData?.job?.title);
-
-////// Nullish Coalescing //////
-const userInput = undefined
-
-const storedData = userInput ?? 'DEFAULT'
-
-console.log(storedData);
-
-
+const names: Readonly<string[]> = ['Joel', 'Yesenia']
+// names.push('Faith') // Not allowed with Readonly type
+// names.pop()
